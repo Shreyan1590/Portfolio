af04146c4e873a625009a8d5855342fd19d2ecce
Shreyan S <shreyanofficial25@gmail.com>
2025-08-06 16:28:21 +0000

    Generation Failed Failed to get a greeting from the AI: FAILED_PRECONDIT

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,7 @@
 
-import { config } from 'dotenv';
-config();
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [googleAI()],
 });
